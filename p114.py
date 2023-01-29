b21f345e736a45f3d1c6bfba29a08e435eecf732
Aley-H <97339740+Aley-H@users.noreply.github.com>
2023-01-29 13:21:14 +0000

    Add files via upload

---
+++ p114.py
@@ -0,0 +1,14 @@
+a = 33
+b = 33
+if b > a:
+    print("b is greater thatn a")
+elif a == b:
+    print("a and b are equal")
+#output is a and b are equal because a and b = 33
+
+def my_function(fname):
+    print(fname + "Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+#this prints all the peoples names with the surname refsnes
